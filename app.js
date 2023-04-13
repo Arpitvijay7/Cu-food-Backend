1edfe96edc0f4bd35a03e50801106399ac3fb8a1
Gzie3009 <108942030+Gzie3009@users.noreply.github.com>
2023-04-13 21:09:30 +0530

    cors added

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+const cors=require("cors")
 const errorMiddleware = require('./middleware/error.js')
 const cookieParser = require("cookie-parser");
 app.use(express.json());
-
+app.use(cors())
 app.use(express.urlencoded({
     extended: true,
 }));
