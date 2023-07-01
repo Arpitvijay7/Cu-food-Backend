74261c609b999dd3ac58fcc9dd1f57511089f313
Arpit <arpitvijay2002@gmail.com>
2023-07-01 13:13:53 +0530

    Fixing bugs

--- utils/automaticClosingOpening.js
+++ utils/automaticClosingOpening.js
@@ -5,33 +5,20 @@ exports.automaticClosingOpening = catchAsyncError(async (req, res, next) => {
   const shop = await Shop.find();
   const date = new Date();
   const time = date.getHours() + ":" + date.getMinutes();
-  console.log(time);
   for (let i = 0; i < shop.length; i++) {
     if (time > shop[i].closeAt) {
       shop[i].status = "closed";
-      console.log('first');
+
       await shop[i].save();
     } else if (time < shop[i].openAt) {
       shop[i].status = "closed";
-      console.log('second');
+
       await shop[i].save();
     } else {
       shop[i].status = "open";
-      console.log('third');
+
       await shop[i].save();
     }
   }
 });
-
-// how to compare two time 
-// const date = new Date();
-// const time = date.getHours() + ":" + date.getMinutes();
-// console.log(time);
-// if (time > "10:00") {
-//   console.log("greater");        // greater      
-// } else if (time < "10:00") {
-//   console.log("smaller");
-// } else {
-//   console.log("equal");
-// }
 
