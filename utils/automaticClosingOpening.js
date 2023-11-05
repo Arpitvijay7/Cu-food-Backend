01f01f920d0e5a277e06d7b883abd372728dd64d
Arpitvijay7 <arpitvijay2002@gmail.com>
2023-11-06 04:42:09 +0530

    Buf fixes

--- utils/automaticClosingOpening.js
+++ utils/automaticClosingOpening.js
@@ -1,22 +1,27 @@
 const catchAsyncError = require("../middleware/catchAsyncError");
 const Food = require("../models/Food");
 const Shop = require("../models/Shop");
-
+const moment = require('moment-timezone');
 exports.automaticClosingOpening = catchAsyncError(async (req, res, next) => {
   const shop = await Shop.find();
-  const date = new Date();
-  const hours = date.getHours().toString().padStart(2, '0');
-  const minutes = date.getMinutes().toString().padStart(2, '0');
-  const time = hours + ":" + minutes;
-
+  // const date = new Date();
+  // const hours = date.getHours().toString().padStart(2, '0');
+  // const minutes = date.getMinutes().toString().padStart(2, '0');
+  // const time = hours + ":" + minutes;
+  
+  const date = moment().tz('Asia/Kolkata'); // Get Indian time
+  const time = date.format('HH:mm'); // Format as HH:mm
   for (let i = 0; i < shop.length; i++) {
+    
     if (time >= shop[i].closeAt || time < shop[i].openAt) {
+
       if (shop[i].status === "open") {
         shop[i].status = "closed";
         await shop[i].save();
       }
     }
     if (time >= shop[i].openAt && time < shop[i].closeAt) {
+      console.log("Inside else");
       if (shop[i].status === "closed") {
         shop[i].status = "open";
 
